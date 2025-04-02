6ab8b36d2e90600b1c31ea7e3a308131d3e2da20
Juan Rogelio Franco Martinez <jfranco@linko.mx>
2025-04-01 22:40:44 -0600

    init upload project

---
+++ android/app/src/main/kotlin/com/example/flutter_contador/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_contador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Center(
+        child: Text('¡Hola Mundo!'),
+      ),
+    );
+  }
+}
